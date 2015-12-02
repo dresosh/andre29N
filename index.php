4d855ba919f2947f59453f147a0bde0310e9cfa5
Dre Sosh <contact@dresosh.com>
2015-12-01 18:58:51 -0800

    changes index.php to only include content as partials

--- index.php
+++ index.php
@@ -1,20 +1,46 @@
-<?php include "./php/head.php"; ?>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>29 Navy</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
 
-<?php include "./php/nav.php"; ?>
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+
+      <?php include "./php/nav.php"; ?>
 
 
-<div class="container-fluid">
+      <div class="container-fluid">
 
-  <?php
-    include "./php/location.php";
-    include "./php/gallery.php";
-    include "./php/lofts.php";
-    include "./php/residents.php";
-    include "./php/team.php";
-    include "./php/contact.php";
-  ?>
+        <?php
+          include "./php/location.php";
+          include "./php/gallery.php";
+          include "./php/lofts.php";
+          include "./php/residents.php";
+          include "./php/team.php";
+          include "./php/contact.php";
+        ?>
 
-</div>
+      </div>
 
-
-<?php include "./php/foot.php"; ?>
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+      <script src="js/vendor/bootstrap.min.js"></script>
+      <script src="js/main.js"></script>
+      <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+      <script>
+          (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+          function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+          e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+          e.src='//www.google-analytics.com/analytics.js';
+          r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+          ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+      </script>
+    </body>
+</html>
