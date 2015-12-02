ea8c9d4884da4e3bd9750f500fff93b94c001f87
dresosh <contact@dresosh.com>
2015-12-01 18:34:41 -0800

    adds buttons to residents page

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include "./php/head.php"; ?>
+
+<?php include "./php/nav.php"; ?>
+
+
+<div class="container-fluid">
+
+  <?php
+    include "./php/location.php";
+    include "./php/gallery.php";
+    include "./php/lofts.php";
+    include "./php/residents.php";
+    include "./php/team.php";
+    include "./php/contact.php";
+  ?>
+
+</div>
+
+
+<?php include "./php/foot.php"; ?>
