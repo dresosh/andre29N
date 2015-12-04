6635a608428d422e161dcbd6b7b047aa57cf908f
dresosh <contact@dresosh.com>
2015-12-04 15:15:05 -0800

    adds gallery and creates modal for residents page

--- php/gallery.php
+++ php/gallery.php
@@ -14,12 +14,12 @@
           <!-- Wrapper for slides -->
           <div class="carousel-inner" role="listbox">
             <div class="item active">
-              <img src="./img/contactImg.jpg" alt="...">
+              <img src="./img/gallery1.png" alt="...">
               <div class="carousel-caption">
               </div>
             </div>
             <div class="item">
-              <img src="./img/contactImg.jpg" alt="...">
+              <img src="./img/gallery1.png" alt="...">
               <div class="carousel-caption">
               </div>
             </div>
